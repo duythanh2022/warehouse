acac9775370e2316c90699fe6f442e5bfb4ee5bd
thanh <duythanhlk1998@gmail.com>
2022-09-21 22:57:15 +0700

    fix error cart

--- src/controllers/cart.controller.js
+++ src/controllers/cart.controller.js
@@ -21,7 +21,6 @@ const cartCtrl = {
           },
         }
       );
-      console.log("wwww",stock);
       if(stock.modifiedCount){
         // addd to cart
         const addToCart = await Cart.findOneAndUpdate({
@@ -32,7 +31,6 @@ const cartCtrl = {
                 quantity
             }
         })
-        console.log("momo",addToCart)
         return 1
       }
     } catch (error) {
--- src/routes/cart.router.js
+++ src/routes/cart.router.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const router = express.Router();
 const cartCtrl = require('../controllers/cart.controller')
-router.post('/cart',cartCtrl.addToCart)
+router.put('/cart',cartCtrl.addToCart)
 module.exports = router
